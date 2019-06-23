ce906f0c49b51d05984f483bf81ecc3aff93b1f0
Jorge Acosta <jh.acostadelapena@gmail.com>
2019-06-23 00:21:03 -0400

    last commit

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import App from './app';
 import { store } from './components/store/index';
 // establishes socket connection
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
